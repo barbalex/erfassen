6bad434c61fbf748e4a2c6722b9b9187543404a3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-28 15:17:23 +0200

    remove unneeded

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -32,7 +32,7 @@ const Layout = ({ children }: Props) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header />
         <div
           style={{
             margin: '0 auto',
