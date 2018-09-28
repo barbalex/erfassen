e4ab6fc8e95b16386c6ccaaf8a25d5eec2628f51
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-28 12:46:46 +0200

    get typography to work

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,6 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
 
 interface Props {
   children: JSX.Element[] | JSX.Element
