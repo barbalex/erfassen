a2229235ba3b0977abf99ea9c4e7db5b67bb31fc
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-28 15:34:25 +0200

    work on home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,63 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import Typography from '@material-ui/core/Typography'
+import styled from 'styled-components'
+import MaterialCard from '@material-ui/core/Card'
 
 import Layout from '../components/layout'
 
+const Container = styled.div`
+  padding: 25px;
+`
+const CardContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+  @media (min-width: 700px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (min-width: 1200px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media (min-width: 1700px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`
+const Card = styled(MaterialCard)`
+  padding: 10px;
+`
+const CardTitle = styled.h3``
+
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Container>
+      <Typography align="center" variant="title" color="inherit" noWrap>
+        Erfassen Sie Daten:
+      </Typography>
+      <CardContainer>
+        <Card>
+          <CardTitle>Aller Art</CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>Card 2</CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>Card 3</CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>Card 4</CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>Card 5</CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>Card 6</CardTitle>
+        </Card>
+        <Card>
+          <CardTitle>Card 7</CardTitle>
+        </Card>
+      </CardContainer>
+    </Container>
   </Layout>
 )
 
