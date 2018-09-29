ac0f97ed8123195f8e488eb377c6c1d6e00899cf
Alexander Gabriel <alex.barbalex@gmail.com>
2018-09-29 13:28:02 +0200

    style scrollbars

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,6 +12,31 @@ const GlobalStyle = createGlobalStyle`
   html {
     overflow: auto;
   }
+
+/* scrollbars */
+
+::-webkit-scrollbar {
+  width: 12px;
+  background: rgba(255, 253, 231, 0.1);
+}
+
+::-webkit-scrollbar:horizontal {
+  height: 12px;
+}
+
+::-webkit-scrollbar-track {
+  -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.4);
+}
+
+::-webkit-scrollbar-thumb {
+  border-radius: 2px;
+  -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.5);
+  background: rgba(85, 85, 85, 0.05);
+}
+
+::-webkit-scrollbar-corner {
+  background: rgba(0, 0, 0, 0);
+}
   
 
 /*
