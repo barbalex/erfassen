64d848b888018cb403ff2e1752a9664071fb4a41
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-03 14:20:22 +0200

    optimize

--- src/context/db.tsx
+++ src/context/db.tsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 export const DbContext = React.createContext({ db: null, setDb: () => {} })
 
 // This function takes a component...
-export function withDb(PassedComponent: Component) {
+export function withDb(Component: any) {
   // ...and returns another component...
   return function DbComponent(props: any) {
     // ... and renders the wrapped component with the context theme!
     // Notice that we pass through any additional props as well
     return (
       <DbContext.Consumer>
-        {db => <PassedComponent {...props} db={db} />}
+        {db => <Component {...props} db={db} />}
       </DbContext.Consumer>
     )
   }
