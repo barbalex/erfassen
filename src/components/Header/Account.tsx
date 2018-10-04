2e1d13308d7ad6201ff82c73ea708f10b860e450
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-04 18:10:57 +0200

    work on account

--- src/components/Header/Account.tsx
+++ src/components/Header/Account.tsx
@@ -1,27 +1,59 @@
 import React from 'react'
 import IconButton from '@material-ui/core/IconButton'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
 import { FaUserCircle as UserIcon } from 'react-icons/fa'
 import styled from 'styled-components'
 import { navigate } from 'gatsby'
 import compose from 'recompose/compose'
 import withHandlers from 'recompose/withHandlers'
+import withState from 'recompose/withState'
+
+interface Props {
+  anchorEl: EventTarget | null
+  setAnchorEl: (anchorEl: EventTarget | null) => void
+  onClickMenu: (event: Event) => void
+  onCloseMenu: (event: Event) => void
+}
 
 const enhance = compose(
-  withHandlers({ onClickSiteTitle: () => () => navigate('/') }),
+  withState('anchorEl', 'setAnchorEl', null),
+  withHandlers<any, any>({
+    onClickMenu: ({ setAnchorEl }: Props) => (event: Event) =>
+      setAnchorEl(event.currentTarget),
+    onCloseMenu: ({ setAnchorEl }: Props) => () => setAnchorEl(null),
+  }),
 )
 
-interface Props {}
-
-const Account: React.SFC<Props> = () => (
-  <IconButton
-    aria-haspopup="true"
-    aria-label="Konto"
-    onClick={() => console.log('TODO')}
-    color="inherit"
-    title="Konto"
-  >
-    <UserIcon />
-  </IconButton>
+const Account: React.SFC<Props> = ({ anchorEl, onCloseMenu, onClickMenu }) => (
+  <div>
+    <IconButton
+      aria-haspopup="true"
+      aria-label="Konto"
+      onClick={onClickMenu}
+      color="inherit"
+      title="Konto"
+    >
+      <UserIcon />
+    </IconButton>
+    <Menu
+      id="menu-appbar"
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={!!anchorEl}
+      onClose={onCloseMenu}
+    >
+      <MenuItem onClick={onCloseMenu}>Profile</MenuItem>
+      <MenuItem onClick={onCloseMenu}>My account</MenuItem>
+    </Menu>
+  </div>
 )
 
 export default enhance(Account)
