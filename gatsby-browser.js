c8178177b5b554ac4dc1a4a0ce5f76a391dc74c3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-06 11:19:50 +0200

    refactor initiation to app

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,10 +4,6 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 import React from 'react'
-import { createGlobalStyle } from 'styled-components'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import red from '@material-ui/core/colors/red'
-import teal from '@material-ui/core/colors/teal'
 
 import App from './src/App'
 
@@ -16,82 +12,4 @@ import App from './src/App'
  * need to pass App db state
  * as db needs to be created async
  */
-export const wrapRootElement = ({ element }) => {
-  const GlobalStyle = createGlobalStyle`
-    html {
-      overflow: hidden;
-    }
-
-    /* scrollbars */
-
-    ::-webkit-scrollbar {
-    width: 12px;
-    background: rgba(255, 253, 231, 0.1);
-    }
-
-    ::-webkit-scrollbar:horizontal {
-    height: 12px;
-    }
-
-    ::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.4);
-    }
-
-    ::-webkit-scrollbar-thumb {
-    border-radius: 2px;
-    -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.5);
-    background: rgba(85, 85, 85, 0.05);
-    }
-
-    ::-webkit-scrollbar-corner {
-    background: rgba(0, 0, 0, 0);
-    }
-
-
-    /*
-    * seems that overflow:hidden is necessary
-    * for all relfex-elements
-    * BUT: map is not shown with it...
-    */
-
-    .reflex-element:not(.karte) {
-    border-right: 0 !important;
-    border-left: 0 !important;
-    }
-
-    /*
-    .reflex-element:not(.karte) > div {
-    overflow: hidden;
-    }*/
-
-    .reflex-splitter {
-    background-color: #fffde7 !important;
-    border-right: 1px solid #fffde7 !important;
-    border-left: 1px solid #fffde7 !important;
-    }
-
-    .reflex-splitter:hover {
-    background-color: #fff59d !important;
-    border-right: 1px solid #fff59d !important;
-    border-left: 1px solid #fff59d !important;
-    }
-  `
-  const theme = createMuiTheme({
-    palette: {
-      type: 'light',
-      primary: { main: teal[500] },
-      error: {
-        main: red[500],
-      },
-    },
-  })
-
-  return (
-    <>
-      <GlobalStyle />
-      <MuiThemeProvider theme={theme}>
-        <App element={element} />
-      </MuiThemeProvider>
-    </>
-  )
-}
+export const wrapRootElement = ({ element }) => <App element={element} />
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,82 @@ import React, { Component } from 'react'
 import compose from 'recompose/compose'
 import withState from 'recompose/withState'
 import withLifecycle from '@hocs/with-lifecycle'
+import { createGlobalStyle } from 'styled-components'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import red from '@material-ui/core/colors/red'
+import teal from '@material-ui/core/colors/teal'
 
 import { DbContext } from './context/db'
 import createDb from './utils/createDb'
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    overflow: hidden;
+  }
+
+  /* scrollbars */
+
+  ::-webkit-scrollbar {
+    width: 12px;
+    background: rgba(255, 253, 231, 0.1);
+  }
+
+  ::-webkit-scrollbar:horizontal {
+    height: 12px;
+  }
+
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.4);
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 2px;
+    -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.5);
+    background: rgba(85, 85, 85, 0.05);
+  }
+
+  ::-webkit-scrollbar-corner {
+    background: rgba(0, 0, 0, 0);
+  }
+
+
+  /*
+  * seems that overflow:hidden is necessary
+  * for all relfex-elements
+  * BUT: map is not shown with it...
+  */
+
+  .reflex-element:not(.karte) {
+    border-right: 0 !important;
+    border-left: 0 !important;
+  }
+
+  /*
+  .reflex-element:not(.karte) > div {
+  overflow: hidden;
+  }*/
+
+  .reflex-splitter {
+    background-color: #fffde7 !important;
+    border-right: 1px solid #fffde7 !important;
+    border-left: 1px solid #fffde7 !important;
+  }
+
+  .reflex-splitter:hover {
+    background-color: #fff59d !important;
+    border-right: 1px solid #fff59d !important;
+    border-left: 1px solid #fff59d !important;
+  }
+`
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: { main: teal[800] },
+    error: {
+      main: red[800],
+    },
+  },
+})
 
 const enhance = compose(
   withState('db', 'setDb', null),
@@ -22,7 +95,14 @@ const enhance = compose(
 )
 
 const App = ({ db, element }: { db: any; element: Component }) => (
-  <DbContext.Provider value={db}>{element}</DbContext.Provider>
+  <DbContext.Provider value={db}>
+    <MuiThemeProvider theme={theme}>
+      <>
+        <GlobalStyle />
+        {element}
+      </>
+    </MuiThemeProvider>
+  </DbContext.Provider>
 )
 
 export default enhance(App)
