e5b1fea75d12167eb3e6ac271132a8df60c16b0c
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-06 20:47:06 +0200

    modularize

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,12 @@
 import React from 'react'
-import { createGlobalStyle } from 'styled-components'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import red from '@material-ui/core/colors/red'
 import teal from '@material-ui/core/colors/teal'
 import { Provider as StateProvider } from 'unstated'
 
-const GlobalStyle = createGlobalStyle`
-  html {
-    overflow: hidden;
-  }
+import createGlobalStyle from './utils/createGlobalStyle'
 
-  /* scrollbars */
-
-  ::-webkit-scrollbar {
-    width: 12px;
-    background: rgba(255, 253, 231, 0.1);
-  }
-
-  ::-webkit-scrollbar:horizontal {
-    height: 12px;
-  }
-
-  ::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.4);
-  }
-
-  ::-webkit-scrollbar-thumb {
-    border-radius: 2px;
-    -webkit-box-shadow: inset 0 0 7px rgba(0, 0, 0, 0.5);
-    background: rgba(85, 85, 85, 0.05);
-  }
-
-  ::-webkit-scrollbar-corner {
-    background: rgba(0, 0, 0, 0);
-  }
-
-
-  /*
-  * seems that overflow:hidden is necessary
-  * for all relfex-elements
-  * BUT: map is not shown with it...
-  */
-
-  .reflex-element:not(.karte) {
-    border-right: 0 !important;
-    border-left: 0 !important;
-  }
-
-  /*
-  .reflex-element:not(.karte) > div {
-  overflow: hidden;
-  }*/
-
-  .reflex-splitter {
-    background-color: #fffde7 !important;
-    border-right: 1px solid #fffde7 !important;
-    border-left: 1px solid #fffde7 !important;
-  }
-
-  .reflex-splitter:hover {
-    background-color: #fff59d !important;
-    border-right: 1px solid #fff59d !important;
-    border-left: 1px solid #fff59d !important;
-  }
-`
+const GlobalStyle = createGlobalStyle()
 const theme = createMuiTheme({
   palette: {
     type: 'light',
