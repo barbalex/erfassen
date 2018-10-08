69cd54bd37c91416474091182e84bcbe041a8ae9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-08 23:09:20 +0200

    add props export

--- src/state/RxDb.ts
+++ src/state/RxDb.ts
@@ -2,8 +2,12 @@ import { Container } from 'unstated'
 
 import createRxDb from '../utils/createRxDb'
 
-interface Props {
+interface StateProps {
   rxDb: any
+}
+export interface Props {
+  state: StateProps
+  setRxDb: (rxDb: any) => void
 }
 declare global {
   interface Window {
@@ -17,8 +21,8 @@ declare global {
  * so need to set it on window and fetch it from there
  * to enable nice dev experience
  */
-export default class RxDbContainer extends Container<Props> {
-  constructor(props: Props) {
+export default class RxDbContainer extends Container<StateProps> {
+  constructor(props: StateProps) {
     super()
     const rxDb = typeof window === 'undefined' ? null : window.rxDb || null
     this.state = {
