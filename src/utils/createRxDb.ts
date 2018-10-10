63b9dbdcc0b528296bf9c83ee7efb0402420afb2
Alexander Gabriel <alex.barbalex@gmail.com>
2018-10-10 18:35:51 +0200

    return syncs value also

--- src/utils/createRxDb.ts
+++ src/utils/createRxDb.ts
@@ -7,20 +7,24 @@ import zeitSchema from '../schemas/zeit.json'
 import ortSchema from '../schemas/ort.json'
 import beobSchema from '../schemas/beob.json'
 import messageSchema from '../schemas/message.json'
+import { Props } from '../state/RxDb'
 
 rxdb.plugin(pouchdbAdapterHttp)
 rxdb.plugin(pouchdbAdapterIdb)
 
 export default async () => {
+  // TODO:
+  // 1. fetch userDoc and it's project list
+  // 2. for every project create an rxdb
+  // 3. for every project fetch typeDefs doc and extract table list
+  // 4. for every table create collection and sync it
+  let syncs: any = {}
   let db
   try {
     db = await rxdb.create({
       name: 'erfassen',
       adapter: 'idb',
       queryChangeDetection: false, // <- queryChangeDetection (optional, default: false)
-      // only needed in dev mode
-      // because db is rebuilt on every live reload
-      //ignoreDuplicate: true,
     })
   } catch (error) {
     throw error
@@ -40,7 +44,13 @@ export default async () => {
     name: 'ort',
     schema: ortSchema,
   })
-  db.ort.sync({
+  /**
+   * create global sync object
+   * and pass it sync responces
+   * reason: be able to check if replication exists already
+   * before starting new one
+   */
+  syncs.ort = await db.ort.sync({
     remote: 'http://localhost:5984/erfassen/',
     options: {
       live: true,
@@ -55,7 +65,7 @@ export default async () => {
     name: 'beob',
     schema: beobSchema,
   })
-  db.beob.sync({
+  syncs.beob = db.beob.sync({
     remote: 'http://localhost:5984/erfassen/',
     options: {
       live: true,
@@ -67,5 +77,5 @@ export default async () => {
       .eq('beob'),
   })
 
-  return db
+  return { db, syncs }
 }
