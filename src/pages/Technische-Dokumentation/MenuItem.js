e32ffb170c31fa99f6f60f3b0c1f994564e631c3
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-11-04 13:35:37 +0100

    use hooks

--- src/pages/Technische-Dokumentation/MenuItem.js
+++ src/pages/Technische-Dokumentation/MenuItem.js
@@ -1,33 +1,33 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { navigate } from 'gatsby'
 import MListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Divider from '@material-ui/core/Divider'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
 import get from 'lodash/get'
+
+import { setConfig } from 'react-hot-loader'
+setConfig({ pureSFC: true })
 
 import ErrorBoundary from '../../components/ErrorBoundary'
 
-const enhance = compose(
-  withHandlers({
-    onClickMenuItem: ({ post }) => () => {
-      navigate(`${post.frontmatter.path}/`)
-    },
-  }),
-)
+const TechnDokuMenuItem = ({ post }) => {
+  const onClickMenuItem = useCallback(
+    () => navigate(`${post.frontmatter.path}/`),
+    [post],
+  )
 
-const TechnDokuMenuItem = ({ post, onClickMenuItem }) => (
-  <ErrorBoundary>
-    <>
-      <MListItem button onClick={onClickMenuItem}>
-        <ListItemText onClick={onClickMenuItem}>
-          {get(post, 'frontmatter.title', '(Titel fehlt)')}
-        </ListItemText>
-      </MListItem>
-      <Divider />
-    </>
-  </ErrorBoundary>
-)
+  return (
+    <ErrorBoundary>
+      <>
+        <MListItem button onClick={onClickMenuItem}>
+          <ListItemText onClick={onClickMenuItem}>
+            {get(post, 'frontmatter.title', '(Titel fehlt)')}
+          </ListItemText>
+        </MListItem>
+        <Divider />
+      </>
+    </ErrorBoundary>
+  )
+}
 
-export default enhance(TechnDokuMenuItem)
+export default TechnDokuMenuItem
