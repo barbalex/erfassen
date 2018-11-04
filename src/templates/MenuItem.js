698cd53be497f77cf3d8cddcb4d6b5018660bcdb
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-11-04 13:38:20 +0100

    use hooks

--- src/templates/MenuItem.js
+++ src/templates/MenuItem.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { navigate } from 'gatsby'
 import MListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import Divider from '@material-ui/core/Divider'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
 import { Location } from '@reach/router'
 import styled from 'styled-components'
 import get from 'lodash/get'
+
+import { setConfig } from 'react-hot-loader'
+setConfig({ pureSFC: true })
 
 import ErrorBoundary from '../components/ErrorBoundary'
 
@@ -16,35 +17,34 @@ const ListItem = styled(MListItem)`
     props.active === 'true' ? '#eaeaea' : 'unset'} !important;
 `
 
-const enhance = compose(
-  withHandlers({
-    onClickMenuItem: ({ post }) => () => {
-      navigate(`${post.frontmatter.path}/`)
-    },
-  }),
-)
+const TechnDokuMenuItem = ({ post }) => {
+  const onClickMenuItem = useCallback(
+    () => navigate(`${post.frontmatter.path}/`),
+    [post],
+  )
 
-const TechnDokuMenuItem = ({ post, onClickMenuItem }) => (
-  <Location>
-    {({ location }) => {
-      const active = (
-        `${post.frontmatter.path}/` === location.pathname
-      ).toString()
+  return (
+    <Location>
+      {({ location }) => {
+        const active = (
+          `${post.frontmatter.path}/` === location.pathname
+        ).toString()
 
-      return (
-        <ErrorBoundary>
-          <>
-            <ListItem button onClick={onClickMenuItem} active={active}>
-              <ListItemText onClick={onClickMenuItem}>
-                {get(post, 'frontmatter.title', '(Titel fehlt)')}
-              </ListItemText>
-            </ListItem>
-            <Divider />
-          </>
-        </ErrorBoundary>
-      )
-    }}
-  </Location>
-)
+        return (
+          <ErrorBoundary>
+            <>
+              <ListItem button onClick={onClickMenuItem} active={active}>
+                <ListItemText onClick={onClickMenuItem}>
+                  {get(post, 'frontmatter.title', '(Titel fehlt)')}
+                </ListItemText>
+              </ListItem>
+              <Divider />
+            </>
+          </ErrorBoundary>
+        )
+      }}
+    </Location>
+  )
+}
 
-export default enhance(TechnDokuMenuItem)
+export default TechnDokuMenuItem
