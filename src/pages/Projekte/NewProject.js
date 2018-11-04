5055e0a344fe5bf8aa11999b1c62758d60380485
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2018-11-04 13:20:16 +0100

    use hooks

--- src/pages/Projekte/NewProject.js
+++ src/pages/Projekte/NewProject.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogActions from '@material-ui/core/DialogActions'
@@ -9,12 +9,12 @@ import FormControl from '@material-ui/core/FormControl'
 import FormHelperText from '@material-ui/core/FormHelperText'
 import Button from '@material-ui/core/Button'
 import styled from 'styled-components'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
-import withState from 'recompose/withState'
 import rxdb from 'rxdb'
 import pouchdbAdapterHttp from 'pouchdb-adapter-http'
 import pouchdbAdapterIdb from 'pouchdb-adapter-idb'
+
+import { setConfig } from 'react-hot-loader'
+setConfig({ pureSFC: true })
 
 import ErrorBoundary from '../../components/ErrorBoundary'
 import withAuthState from '../../state/withAuth'
@@ -44,137 +44,114 @@ const StyledFormHelperText = styled(FormHelperText)`
   }
 `
 
-const enhance = compose(
-  withAuthState,
-  withState('projectName', 'setProjectName', ''),
-  withState('projectName2', 'setProjectName2', ''),
-  withState('nameHelperText', 'setNameHelperText', ''),
-  withState('name2HelperText', 'setName2HelperText', ''),
-  withHandlers({
-    close: ({ setNewProjectOpen }) => () => setNewProjectOpen(false),
-    onClickCreate: ({
-      projectName,
-      projectName2,
-      authState,
-      setNameHelperText,
-      setName2HelperText,
-      setNewProjectOpen,
-    }) => async () => {
-      // ensure names are equal
-      if (projectName !== projectName2) {
-        return setNameHelperText('Die Namen müssen übereinstimmen')
-      }
-      // ensure projectName is valid for couch db
-      if (!/^[a-z][a-z0-9_$()+/-]*$/.test(projectName)) {
-        return setNameHelperText(
-          <>
-            <div>Dieser Name ist leider nicht zulässig.</div>
-            <div>Regeln für Namen:</div>
+const NewProject = ({ newProjectOpen, setNewProjectOpen, authState }) => {
+  const [projectName, setProjectName] = useState('')
+  const [projectName2, setProjectName2] = useState('')
+  const [nameHelperText, setNameHelperText] = useState('')
+  const [name2HelperText, setName2HelperText] = useState('')
+
+  const close = useCallback(() => setNewProjectOpen(false))
+  const onClickCreate = useCallback(async () => {
+    // ensure names are equal
+    if (projectName !== projectName2) {
+      return setNameHelperText('Die Namen müssen übereinstimmen')
+    }
+    // ensure projectName is valid for couch db
+    if (!/^[a-z][a-z0-9_$()+/-]*$/.test(projectName)) {
+      return setNameHelperText(
+        <>
+          <div>Dieser Name ist leider nicht zulässig.</div>
+          <div>Regeln für Namen:</div>
+          <ul>
+            <li>Beginnt mit einem Buchstaben</li>
+            <li>Zulässig sind folgende Zeichen:</li>
             <ul>
-              <li>Beginnt mit einem Buchstaben</li>
-              <li>Zulässig sind folgende Zeichen:</li>
-              <ul>
-                <li>Buchstaben</li>
-                <li>Zahlen</li>
-                <li>{`_, $, (, ), +, / und -`}</li>
-              </ul>
+              <li>Buchstaben</li>
+              <li>Zahlen</li>
+              <li>{`_, $, (, ), +, / und -`}</li>
             </ul>
-          </>,
-        )
-      }
-      // create new project
-      try {
-        await createProjectDb({ projectName, authState })
-      } catch (error) {
-        setNameHelperText(error.message)
-        return setName2HelperText(error.message)
-      }
-      setNameHelperText('')
-      setNewProjectOpen(false)
-    },
-  }),
-  withHandlers({
-    onBlurName: ({ setProjectName }) => event =>
-      setProjectName(event.target.value),
-    onBlurName2: ({ setProjectName2 }) => event =>
-      setProjectName2(event.target.value),
-  }),
-)
+          </ul>
+        </>,
+      )
+    }
+    // create new project
+    try {
+      await createProjectDb({ projectName, authState })
+    } catch (error) {
+      setNameHelperText(error.message)
+      return setName2HelperText(error.message)
+    }
+    setNameHelperText('')
+    setNewProjectOpen(false)
+  })
+  const onBlurName = useCallback(event => setProjectName(event.target.value))
+  const onBlurName2 = useCallback(event => setProjectName2(event.target.value))
 
-const NewProject = ({
-  projectName,
-  projectName2,
-  nameHelperText,
-  name2HelperText,
-  onBlurName,
-  onBlurName2,
-  onClickCreate,
-  close,
-  newProjectOpen,
-}) => (
-  <ErrorBoundary>
-    <StyledDialog aria-labelledby="login-dialog-title" open={newProjectOpen}>
-      <DialogTitle id="login-dialog-title">Neues Projekt</DialogTitle>
-      <StyledDiv>
-        <FormControl
-          error={!!nameHelperText}
-          fullWidth
-          aria-describedby="nameHelper"
-          required={true}
-        >
-          <InputLabel htmlFor="projectName">Name</InputLabel>
-          <StyledInput
-            id="projectName"
-            defaultValue=""
-            onBlur={onBlurName}
-            autoFocus
-            autoComplete="off"
-            autoCorrect="off"
-            spellCheck={false}
-          />
-          <StyledFormHelperText id="nameHelper" component="div">
-            {nameHelperText}
-          </StyledFormHelperText>
-        </FormControl>
-        <FormControl
-          error={!!name2HelperText}
-          fullWidth
-          aria-describedby="loginName2Helper"
-          required={true}
-        >
-          <InputLabel htmlFor="projectName2">Name</InputLabel>
-          <StyledInput
-            id="projectName2"
-            type="text"
-            defaultValue=""
-            onBlur={onBlurName2}
-            autoComplete="off"
-            autoCorrect="off"
-            spellCheck={false}
-          />
-          <StyledFormHelperText id="loginName2Helper" component="div">
-            {name2HelperText || (
-              <>
-                <span>Bitte den Namen prüfen und nochmals eingeben.</span>
-                <br />
-                <span>Er kann danach nicht mehr verändert werden</span>
-              </>
-            )}
-          </StyledFormHelperText>
-        </FormControl>
-      </StyledDiv>
-      <DialogActions>
-        <Button onClick={close}>abbrechen</Button>
-        <Button
-          color="primary"
-          onClick={onClickCreate}
-          disabled={!(projectName && projectName2)}
-        >
-          Projekt erstellen
-        </Button>
-      </DialogActions>
-    </StyledDialog>
-  </ErrorBoundary>
-)
+  return (
+    <ErrorBoundary>
+      <StyledDialog aria-labelledby="login-dialog-title" open={newProjectOpen}>
+        <DialogTitle id="login-dialog-title">Neues Projekt</DialogTitle>
+        <StyledDiv>
+          <FormControl
+            error={!!nameHelperText}
+            fullWidth
+            aria-describedby="nameHelper"
+            required={true}
+          >
+            <InputLabel htmlFor="projectName">Name</InputLabel>
+            <StyledInput
+              id="projectName"
+              defaultValue=""
+              onBlur={onBlurName}
+              autoFocus
+              autoComplete="off"
+              autoCorrect="off"
+              spellCheck={false}
+            />
+            <StyledFormHelperText id="nameHelper" component="div">
+              {nameHelperText}
+            </StyledFormHelperText>
+          </FormControl>
+          <FormControl
+            error={!!name2HelperText}
+            fullWidth
+            aria-describedby="loginName2Helper"
+            required={true}
+          >
+            <InputLabel htmlFor="projectName2">Name</InputLabel>
+            <StyledInput
+              id="projectName2"
+              type="text"
+              defaultValue=""
+              onBlur={onBlurName2}
+              autoComplete="off"
+              autoCorrect="off"
+              spellCheck={false}
+            />
+            <StyledFormHelperText id="loginName2Helper" component="div">
+              {name2HelperText || (
+                <>
+                  <span>Bitte den Namen prüfen und nochmals eingeben.</span>
+                  <br />
+                  <span>Er kann danach nicht mehr verändert werden</span>
+                </>
+              )}
+            </StyledFormHelperText>
+          </FormControl>
+        </StyledDiv>
+        <DialogActions>
+          <Button onClick={close}>abbrechen</Button>
+          <Button
+            color="primary"
+            onClick={onClickCreate}
+            disabled={!(projectName && projectName2)}
+          >
+            Projekt erstellen
+          </Button>
+        </DialogActions>
+      </StyledDialog>
+    </ErrorBoundary>
+  )
+}
 
-export default enhance(NewProject)
+export default withAuthState(NewProject)
